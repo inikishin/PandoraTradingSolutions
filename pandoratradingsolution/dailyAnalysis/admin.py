02a155996f5c42a18c98d286b53acf419375ea52
ilya <inikishin@gmail.com>
2020-07-02 16:46:12 +0300

    change admin view for daily analysis

--- pandoratradingsolution/dailyAnalysis/admin.py
+++ pandoratradingsolution/dailyAnalysis/admin.py
@@ -7,5 +7,6 @@ admin.site.register(AnalysisType)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['header', 'slug', 'ticker', 'created', 'sig_elder', 'sig_channel', 'sig_DivBar', 'sig_NR4ID', 'sig_breakVolatility']
     list_filter = ['created', 'ticker']
+    search_fields = ['header']
 
 admin.site.register(Post, PostAdmin)
