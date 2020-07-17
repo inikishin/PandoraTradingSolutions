ac1e0472db57a3a526eda25ea42dae775c256544
ilya <inikishin@gmail.com>
2020-07-17 17:45:48 +0300

    sign up form design updated

--- pandoratradingsolution/pandoratradingsolution/views.py
+++ pandoratradingsolution/pandoratradingsolution/views.py
@@ -22,19 +22,21 @@ def profile_view(request):
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+        print("1")
         if form.is_valid():
+            print("2")
             new_user = form.save()
             new_user.first_name = form.cleaned_data['first_name']
             new_user.last_name = form.cleaned_data['last_name']
             new_user.email = form.cleaned_data['email']
             new_user.save()
-            return redirect('profile')
+            return redirect('mainpage')
         else:
             form = SignUpForm(request.POST)
-            return render(request, 'pandoratradingsolution/signup.html', {'form': form})
+            return render(request, 'pandoratradingsolution/ms_signup.html', {'form': form})
     else:
         form = SignUpForm()
-        return render(request, 'pandoratradingsolution/signup.html', {'form': form})
+        return render(request, 'pandoratradingsolution/ms_signup.html', {'form': form})
 
 def contact(request):
     return render(request, 'pandoratradingsolution/ms_contact.html')
