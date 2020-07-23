121b32e2d5a96a4273dfa2337d34bacf7361c7c4
ilya <inikishin@gmail.com>
2020-07-23 15:32:52 +0300

    new field to model marketDict - ticker_name

--- pandoratradingsolution/marketDictionary/admin.py
+++ pandoratradingsolution/marketDictionary/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Ticker, Market
 
-admin.site.register(Ticker)
 admin.site.register(Market)
+
+class TickerAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'ticker_name', 'full_name', 'market', 'site']
+    list_filter = ['market', 'short_name']
+    search_fields = ['ticker_name']
+
+admin.site.register(Ticker, TickerAdmin)
+
