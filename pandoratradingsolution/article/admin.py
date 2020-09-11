7527a8de1b2aa918f77c6c9b8bf3f978d3fc9f96
ilya <inikishin@gmail.com>
2020-09-11 10:29:05 +0300

    clearing article app

--- pandoratradingsolution/article/admin.py
+++ pandoratradingsolution/article/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from .models import Article
 
+
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('custom_fLate_page', 'meta_description', 'meta_keywords', 'date_pub', 'author')
+    list_display = ('custom_flatpage', 'meta_description', 'meta_keywords', 'date_pub', 'author')
+
 
 admin.site.register(Article, ArticleAdmin)
-		
