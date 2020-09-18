aec0026eb98ee0314504e877d985465c1fce6313
ilya <inikishin@gmail.com>
2020-09-18 12:38:30 +0300

    slug_url for dailyAnalysis/ step 1

--- pandoratradingsolution/dailyAnalysis/urls.py
+++ pandoratradingsolution/dailyAnalysis/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='dailyAnalysis'),
     path('<int:post_id>/', views.detail, name='detail'),
+    path('<slug:post_slug>/', views.detail_slug, name='detail'),
     path('api/posts/', views.PostView.as_view()),
 ]
