1f49c5d28bd9d9b42a4e5950bd03950ef107e022
Ilya Nikishin <inikishin@gmail.com>
2020-10-30 07:26:13 +0300

    add new url in da: dailyanalysis/moex

--- pandoratradingsolution/dailyAnalysis/urls.py
+++ pandoratradingsolution/dailyAnalysis/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dailyAnalysis'),
+    path('', views.index, name='dailyAnalysis'), # TODO: delete after redirect
+    path('moex/', views.index, name='dailyAnalysis'),
     path('<slug:post_slug>/', views.detail_slug, name='detail'),
     path('api/posts/', views.PostView.as_view()),
 ]
