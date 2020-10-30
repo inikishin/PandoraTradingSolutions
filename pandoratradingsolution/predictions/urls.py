a1e369a07a4ec43556a945e678b87907638e4c94
Ilya Nikishin <inikishin@gmail.com>
2020-10-30 18:59:41 +0300

    add /moex/ to predictions

--- pandoratradingsolution/predictions/urls.py
+++ pandoratradingsolution/predictions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='predictions'),
+    path('moex/', views.index, name='predictions'),
     path('api/predictions/', views.PredictionView.as_view()),
     path('api/predictionhorizons/', views.PredictionHorizonView.as_view()),
 ]
