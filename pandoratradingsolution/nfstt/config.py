9551014188a40876dee8ca176b8ec0430958e5af
Ilya Nikishin <inikishin@gmail.com>
2021-05-24 15:08:54 +0300

    NFSTT updated

--- pandoratradingsolution/nfstt/config.py
+++ pandoratradingsolution/nfstt/config.py
@@ -2,5 +2,5 @@ telbot_token = '1801338330:AAG9goAwoCFHMAvCYOmvF68A35o136AbFgE'
 
 chat_ids = [791932835]
 
-site_descr = {'site_name': 'Pandora Research',
-              'site_url': 'https://pandoraresearch.ru'}
+site_descr = {'site_name': 'Pandora Trading Solutions',
+              'site_url': 'https://pandoratradingsolutions.ru'}
